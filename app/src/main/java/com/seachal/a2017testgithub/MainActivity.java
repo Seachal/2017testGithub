e5abf52c1d170134ec8e0b98935a5e7c8d7f099b
Seachal <zhangxichao100@163.com>
2017-06-20 17:55:45 +0800

    Initial commit

---
+++ app/src/main/java/com/seachal/a2017testgithub/MainActivity.java
@@ -0,0 +1,13 @@
+package com.seachal.a2017testgithub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
